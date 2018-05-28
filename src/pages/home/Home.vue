2554205048c444216b80b4ca9394c052a70fc2ec
xuxiaobin <925838659@qq.com>
2018-05-28 20:41:06 +0800

    change

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 import HomeHeader from './components/header.vue'
+import HomeSwiper from './components/Swiper.vue'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="wrapper">
+  <swiper :options="swiperOption">
+    <swiper-slide v-for="item in swiperList" :key="item.id">
+      <img class="swiper-img" :src="item.imgUrl" alt="Loading">
+    </swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/3b/ef86879aa50e3002.jpg_750x200_2a108508.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/e5/59fad13a64807d02.jpg_750x200_713ae984.jpg'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/e8/14b75b1c81fbe702.jpg_750x200_e6d4f1f1.jpg'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/5f/c08ce08779ff8202.jpg_750x200_3a907189.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~styles/varibles.styl"
+  .wrapper >>> .swiper-pagination-bullet-active
+    background #fff !important
+  .wrapper
+    width 100%
+    height 0
+    overflow hidden
+    padding-bottom 26.7%
+    background #eee
+    .swiper-img
+      width 100%
+</style>
