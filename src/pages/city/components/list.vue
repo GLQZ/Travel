fabb64d2592d2eaa043fa9f9ca25fcb12293902b
xuxiaobin <925838659@qq.com>
2018-06-02 17:27:33 +0800

    performance

--- src/pages/city/components/list.vue
+++ src/pages/city/components/list.vue
@@ -17,7 +17,11 @@
           </div>
         </div>
       </div>
-      <div class="area" v-for="(item, key) in cities" :key="key">
+      <div class="area"
+           v-for="(item, key) in cities"
+           :key="key"
+           :ref="key"
+      >
         <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
           <div
@@ -37,10 +41,19 @@ export default {
   name: 'CityList',
   props: {
     cities: Object,
-    hotCities: Array
+    hotCities: Array,
+    letter: String
   },
   mounted () {
     this.scroll = new BScroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 </script>
