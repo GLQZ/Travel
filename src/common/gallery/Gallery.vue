4d852e6832b61da48d44d18d371096d1d618b6ce
xuxiaobin <925838659@qq.com>
2018-06-20 17:30:55 +0800

    banner finish

---
+++ src/common/gallery/Gallery.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="container" @click="handleGalleryClick">
+    <div class="wrapper">
+      <swiper :options="swiperOption">
+        <swiper-slide
+          v-for="(item, index) in imgs"
+          :key="index"
+        >
+          <img class="gallery-img" :src="item" alt="Loading">
+        </swiper-slide>
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'CommonGallery',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        loop: false,
+        observer: true,
+        observeParents: true
+      }
+    }
+  },
+  methods: {
+    handleGalleryClick () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .container >>> .swiper-container
+    overflow inherit
+  .container
+    display flex
+    flex-direction column
+    justify-content center
+    z-index 99
+    position fixed
+    left 0
+    right 0
+    top 0
+    bottom 0
+    background #000
+    .wrapper
+      height 0
+      width 100%
+      padding-bottom 65%
+      .gallery-img
+        width 100%
+      .swiper-pagination
+        color #fff
+        bottom -1rem
+</style>
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img
+        class="banner-img"
+        src="//img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg"
+        alt="Loading">
+      <div class="banner-info">
+        <div class="banner-title">故宫(AAAAA景区)</div>
+        <div class="banner-map">
+          <span class="iconfont banner-icon icon-map">&#xe618;</span>
+          20
+        </div>
+      </div>
+    </div>
+    <common-gallery
+      :imgs="imgs"
+      v-show="showGallery"
+      @close="handleGalleryClose"
+    ></common-gallery>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import CommonGallery from 'common/gallery/Gallery'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallery: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/1410/9d/fe8109ab5df1c9c324e74284fa802e72.water.jpg_r_800x800_b9a5440a.jpg',
+        'http://img1.qunarzz.com/sight/p0/1410/9d/fe8109ab5df1c9c324e74284fa802e72.water.jpg_r_800x800_b9a5440a.jpg']
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallery = true
+    },
+    handleGalleryClose () {
+      this.showGallery = false
+    }
+  },
+  components: {
+    CommonGallery
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~styles/varibles.styl"
+  .banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    .banner-img
+      width 100%
+    .banner-info
+      color #fff
+      display flex
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height .6rem
+      background-image linear-gradient(top, rgba(0,0,0,0), rgba(0,0,0,.8))
+      .banner-title
+        flex 1
+        font-size .32rem
+        padding 0 .2rem
+      .banner-map
+        line-height .32rem
+        height .32rem
+        margin-top .14rem
+        padding 0 .4rem
+        border-radius .2rem
+        background rgba(0,0,0,.8)
+        font-size .24rem
+        .banner-icon
+          font-size .24rem
+</style>
